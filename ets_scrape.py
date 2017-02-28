72fb1df7b34e9d8b6c7035aed8a55ab6db63b62c
Suraj Pai <surajballambat@gmail.com>
2017-03-01 00:23:56 +0530

    Added Argument Pool

--- ets_scrape.py
+++ ets_scrape.py
@@ -36,13 +36,52 @@ class Issue(object):
             content += issue_content +"\n"
 
 
+class Argument(object):
+    def __init__(self):
+        self.argument = []
+
+    def __iter__(self):
+        return self.argument
+
+    def scrape_argument(self):
+        """
+
+        :return: Scrape ETS Website for Argument Pool
+        """
+        argument_page = requests.get('http://www.ets.org/gre/revised_general/prepare/analytical_writing/argument/pool')
+        soup = BeautifulSoup(argument_page.content, "lxml")
+        # Divider Tag with class "divider-50" separates each topic
+        divider_tag = soup.find("div", "divider-50")
+        # Gets all tags on the same level as div tag. All topics are on the same level as divider 50 tag
+        divider_tag = divider_tag.next_siblings
+        content = " "
+        for tag in divider_tag:
+            tag = BeautifulSoup(str(tag), "lxml")
+
+            if tag.p is not None:
+                issue_content = tag.p.getText()
+            else:
+                issue_content = ""
+
+            if tag.div is not None:
+                specification_tag = str(tag.div['class'][0])
+                if specification_tag == "divider-50":
+                    # Append and advance to the new topic
+                    self.argument.append(content)
+                    content = " "
+
+            content += issue_content + "\n"
+
 def scrape():
     """
     
     :return: 
     """
+
     issue = Issue()
     issue.scrape_issue()
+    argument = Argument()
+    argument.scrape_argument()
 
 
 if __name__ == "__main__":
