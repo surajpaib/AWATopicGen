d09f509abbb917bf990c3818415b8ac5c2bc6d9b
Suraj Pai <surajballambat@gmail.com>
2017-03-01 00:17:29 +0530

    Completed Issue Pool

--- ets_scrape.py
+++ ets_scrape.py
@@ -1,21 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
 
+class Issue(object):
+    def __init__(self):
+        self.issue = []
+
+    def __iter__(self):
+        return self.issue
+
+    def scrape_issue(self):
+        """
+        
+        :return: Scrape ETS Website for Issue Pool
+        """
+        issue_page = requests.get('http://www.ets.org/gre/revised_general/prepare/analytical_writing/issue/pool')
+        soup = BeautifulSoup(issue_page.content, "lxml")
+        divider_tag = soup.find("div", "divider-50")
+        divider_tag = divider_tag.next_siblings
+        content = " "
+        for tag in divider_tag:
+            tag = BeautifulSoup(str(tag),"lxml")
+
+            if tag.p is not None:
+                issue_content = tag.p.getText()
+            else:
+                issue_content = ""
+
+            if tag.div is not None:
+                specification_tag = str(tag.div['class'][0])
+                if specification_tag == "divider-50":
+                    self.issue.append(content)
+                    content = " "
+
+
+            content += issue_content +"\n"
+
+
 def scrape():
     """
     
     :return: 
     """
-    issue_webpage = requests.get('http://www.ets.org/gre/revised_general/prepare/analytical_writing/issue/pool')
-    argument_webpage = requests.get('http://www.ets.org/gre/revised_general/prepare/analytical_writing/argument/pool')
-    issue_topics = BeautifulSoup(issue_webpage.content)
-    issue_topics = issue_topics.find_all('p')
-    for issue in issue_topics:
-        print(issue)
-        i = BeautifulSoup(str(issue))
-        i = i.find('p').getText()
-        print(i)
-        break
+    issue = Issue()
+    issue.scrape_issue()
+
 
 if __name__ == "__main__":
     scrape()
