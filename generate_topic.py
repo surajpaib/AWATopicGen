32fd1156f0122d83d66ba2d624915c4e74361bd9
Suraj Pai <surajballambat@gmail.com>
2017-03-02 00:24:42 +0530

    Added Email option

--- generate_topic.py
+++ generate_topic.py
@@ -1,9 +1,27 @@
 import pickle
+import smtplib
 from random import choice
 
 from clint import arguments
 from clint.textui import colored, puts
 from scrapingmodule.ets_scrape import Issue, Argument
+
+def send_mail(recipients, subject, message):
+    header = 'From: surajballambat@gmail.com\n'
+    header += 'To: {0}\n'.format(",".join(recipients))
+    header += 'Subject: {0}\n'.format(subject)
+    message = header + message
+
+    # Replace with username and password of the account you want to send the mail from
+    username = pickle.loads('credentials.pkl')
+    username = username[0]
+    password = username[1]
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(username, password)
+    problems = server.sendmail('surajballambat@gmail.com', recipients, message)
+    server.quit()
+
 def input_flags():
     args = arguments.Args()
     return str(args.flags[0])
@@ -15,6 +33,7 @@ def load_issue():
 
     random_choice = choice(issues)
     puts(colored.green(random_choice))
+    send_mail(['surajballambat@gmail.com'], 'Issue Topic', random_choice)
     puts(colored.blue("Topics Remaining: {0}".format(len(issues))))
     for i, content in enumerate(issues):
         if random_choice == content:
@@ -35,6 +54,7 @@ def load_arg():
 
     random_choice = choice(arg)
     puts(colored.red(choice(arg)))
+    send_mail(['surajballambat@gmail.com'], 'Argument Topic', random_choice)
     puts(colored.blue("Topics Remaining: {0}".format(len(arg))))
     for i, content in enumerate(arg):
         if random_choice == content:
