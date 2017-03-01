6a68a16180097f948cdabeaff2a9f75adfdc5eb9
Suraj Pai <surajballambat@gmail.com>
2017-03-02 00:51:57 +0530

    improved code quality

--- generate_topic.py
+++ generate_topic.py
@@ -1,17 +1,26 @@
+"""
+Generate Topics
+"""
 import pickle
 import smtplib
 from random import choice
-
 from clint import arguments
 from clint.textui import colored, puts
 from scrapingmodule.ets_scrape import Issue, Argument
 
+
 def send_mail(recipients, subject, message):
+    """
+    Sends Mail
+    :param recipients: List of recipients
+    :param subject: Subject of the mail
+    :param message: Message Body
+    :return: Sends mail to the IDs
+    """
     header = 'From: surajballambat@gmail.com\n'
     header += 'To: {0}\n'.format(",".join(recipients))
     header += 'Subject: {0}\n'.format(subject)
     message = header + message
-
     # Replace with username and password of the account you want to send the mail from
     with open('credentials.pkl', "rb") as fp:
         username = pickle.load(fp)
@@ -20,54 +29,52 @@ def send_mail(recipients, subject, message):
     server = smtplib.SMTP('smtp.gmail.com:587')
     server.starttls()
     server.login(login, password)
-    problems = server.sendmail('surajballambat@gmail.com', recipients, message)
+    server.sendmail('surajballambat@gmail.com', recipients, message)
     server.quit()
 
+
 def input_flags():
+    """
+    Input flags
+    :return: Returns the input flags
+    """
     args = arguments.Args()
     return str(args.flags[0])
 
-def load_issue(recipient_list):
+
+def load(topic_type, recipient_list):
+    """
+    Load Module
+    :param recipient_list: Mail recipient list
+    :return: Sends mail and displays topic
+    """
     # Read File
-    with open('./scrapingmodule/issue_pool.pkl', 'rb') as f:
-        issues = pickle.load(f)
+    with open('./scrapingmodule/{0}_pool.pkl'.format(topic_type), 'rb') as f:
+        topics = pickle.load(f)
 
-    random_choice = choice(issues)
-    puts(colored.green(random_choice))
-    send_mail(recipient_list, 'Issue Topic', random_choice)
-    puts(colored.blue("Topics Remaining: {0}".format(len(issues))))
-    for i, content in enumerate(issues):
+    random_choice = choice(topics)
+    if topic_type == "issue":
+        puts(colored.green(random_choice))
+        send_mail(recipient_list, 'Issue Topic', random_choice)
+    elif topic_type == "argument":
+        puts(colored.red(random_choice))
+        send_mail(recipient_list, 'Argument Topic', random_choice)
+
+    for i, content in enumerate(topics):
         if random_choice == content:
             break
-    issues.remove(issues[i])
-
+    topics.remove(topics[i])
+    puts(colored.blue('Topics Remaining: {0}'.format(len(topics))))
     # Write File
-
-    with open('./scrapingmodule/issue_pool.pkl', 'wb') as fp:
-        pickle.dump(issues, fp)
-
-
-
-
-def load_arg(recipient_list):
-    with open('./scrapingmodule/argument_pool.pkl', 'rb') as f:
-        arg = pickle.load(f)
-
-    random_choice = choice(arg)
-    puts(colored.red(choice(arg)))
-    send_mail(recipient_list, 'Argument Topic', random_choice)
-    puts(colored.blue("Topics Remaining: {0}".format(len(arg))))
-    for i, content in enumerate(arg):
-        if random_choice == content:
-            break
-    arg.remove(arg[i])
-    # Write File
-
-    with open('./scrapingmodule/argument_pool.pkl', 'wb') as fp:
-        pickle.dump(arg, fp)
+    with open('./scrapingmodule/{0}_pool.pkl'.format(topic_type), 'wb') as fp:
+        pickle.dump(topics, fp)
 
 
 def reload_files():
+    """
+    Load Classes from ets_scrape
+    :return: pkl files
+    """
     issue = Issue()
     issue.scrape_issue()
     issue.save()
@@ -75,17 +82,19 @@ def reload_files():
     arg.scrape_argument()
     arg.save()
 
-def run():
 
-    recipient_list = ['surajballambat@gmail.com','nayan.taurian@gmail.com']
+def run():
+    """
+    Main
+    :return: runs the program
+    """
+    recipient_list = ['surajballambat@gmail.com']
     if input_flags() == "--issue":
-        load_issue(recipient_list=recipient_list)
+        load("issue", recipient_list=recipient_list)
     if input_flags() == "--argument":
-        load_arg(recipient_list=recipient_list)
+        load("argument", recipient_list=recipient_list)
     if input_flags() == "--reset":
         reload_files()
-
-
 
 
 if __name__ == "__main__":
